1df2f7b834e52ed40258ead08cf2dc70fb35e15e
birajkarmakar <brnet00@gmail.com>
2014-01-17 13:12:54 +0530

    Fixed link "Choose Language" explicitly selected
    Fixed hyper link error when "Choose Language" is explicitly selected from combo box for different open source project.

--- scripts/selector.js
+++ scripts/selector.js
@@ -40,12 +40,26 @@ function languageChanged(languageCode){
 	var transifex = document.getElementsByClassName('transifex');
 	
 	for (var i = 0; i<kde.length; i++) {
-		kde.item(i).setAttribute("href", "http://l10n.kde.org/team-infos.php?teamcode="+languageCode);
-		console.log(kde.item(i));
+
+		//Update the project URL.
+		if(languageCode == "Choose Language") {
+            		kde.item(i).setAttribute("href", "http://l10n.kde.org/teams-list.php");
+        	}
+		else {
+			kde.item(i).setAttribute("href", "http://l10n.kde.org/team-infos.php?teamcode="+languageCode);
+			console.log(kde.item(i));
+        	}
 	}
 	for (var i = 0; i<gnome.length; i++) {
+
 		//Update the project URL.
-		gnome.item(i).setAttribute("href", "https://l10n.gnome.org/teams/"+languageCode+"/");
+		if(languageCode == "Choose Language") {
+            		gnome.item(i).setAttribute("href", "https://l10n.gnome.org/teams/");
+        	}
+        	else {
+        		gnome.item(i).setAttribute("href", "https://l10n.gnome.org/teams/"+languageCode+"/");
+        	}
+		
 	}
 	for (var i = 0; i<pootle.length; i++) {
 		//Extract Project's URL Slug from URL.
@@ -57,96 +71,41 @@ function languageChanged(languageCode){
 		var url = urlArr.join("/");
 
 		//Update the project URL.
-		pootle.item(i).setAttribute("href", url+'/'+languageCode+"/");
+		if (languageCode == "Choose Language") {
+            		pootle.item(i).setAttribute("href", url);
+        	}
+		else {
+			pootle.item(i).setAttribute("href", url+'/'+languageCode+"/");
+		}
 	}
 	for (var i = 0; i < locamotion.length; i++) {
+
+		//Generate the required URL.
 		var arr = locamotion.item(i).href.split("/");
 		var projectSlug = arr[arr.length - 2];
-		locamotion.item(i).setAttribute("href","http://mozilla.locamotion.org/"+languageCode+"/"+projectSlug+"/");
-	};
+
+		//Update the project URL.
+		if (languageCode == "Choose Language") {
+            		locamotion.item(i).setAttribute("href", "http://mozilla.locamotion.org/projects/"+projectSlug+"/");
+        	}
+		else {
+			locamotion.item(i).setAttribute("href","http://mozilla.locamotion.org/"+languageCode+"/"+projectSlug+"/");
+		}
+	}
+
+
 	for (var i = 0; i<transifex.length; i++) {
 
 		//Extract Project's URL Slug from URL.
 		var arr = transifex.item(i).href.split("/");
 		var projectSlug = arr[5];
-
-		//Update the project URL.
-		transifex.item(i).setAttribute("href", "https://www.transifex.com/projects/p/"+projectSlug+"/"+"language/"+languageCode+"/");
-	}
-	
-	
-	//when you choose "Choose Language" explicitly from Langauge combo box.
-
-	for (var i = 0; i<transifex.length; i++) {
-
-		//Extract Project's URL Slug from URL.
-		var arr = transifex.item(i).href.split("/");
-		var projectSlug = arr[5];
-                var url = arr[7];
-             
-		//Update the project URL.
-		if (url == "Choose%20Language") {
-               
-                        transifex.item(i).setAttribute("href", "https://www.transifex.com/projects/p/"+projectSlug+"/");
-                }
-               
-                
-	}
-	
-	for (var i = 0; i<locamotion.length; i++) {
-
-		//Extract Project's URL Slug from URL.
-		var arr = locamotion.item(i).href.split("/");
-		var projectSlug = arr[4];
-		var url = arr[3];
 		
 		//Update the project URL.
-		if (url == "Choose%20Language") {
-                        locamotion.item(i).setAttribute("href", "http://mozilla.locamotion.org/projects/"+projectSlug+"/");
-                }
-		
-		
-	}
-
-
-	for (var i = 0; i<pootle.length; i++) {
-		//Extract Project's URL Slug from URL.
-		var arr = pootle.item(i).href.split("/");
-		var projectSlug = arr[4];
-		var url=arr[3];
-		var urlArr = pootle.item(i).href.split("/",3);
-		var urlMain = urlArr.join("/");
-		//Generate the required URL.
-		
-		//Update the project URL.
-		if (url == "Choose%20Language") {
-                        pootle.item(i).setAttribute("href", urlMain);
-                }
-		
-	}
-
-	for (var i = 0; i<gnome.length; i++) {
-		//Update the project URL.
-		var arr = gnome.item(i).href.split("/");
-		var url=arr[4];
-
-		//Generate the required URL.
-		
-		//Update the project URL.
-		if(url == "Choose%20Language") {
-                        gnome.item(i).setAttribute("href", "https://l10n.gnome.org/teams/");
-                }
-		
-	}
-
-
-	for (var i = 0; i<kde.length; i++) {
-		var arr = kde.item(i).href.split("=");
-		var url=arr[1];
-
-		if(url == "Choose%20Language") {
-                        kde.item(i).setAttribute("href", "http://l10n.kde.org/teams-list.php");
-                }
-		
+		if (languageCode == "Choose Language") {
+            		transifex.item(i).setAttribute("href", "https://www.transifex.com/projects/p/"+projectSlug+"/");
+        	}
+		else {
+			transifex.item(i).setAttribute("href", "https://www.transifex.com/projects/p/"+projectSlug+"/"+"language/"+languageCode+"/");
+		}
 	}
 }
